faaec0cdcc0559dbfd991d2074df1433414a1a0d
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-07-29 21:30:43 +0530

    Update params.js

--- src/params.js
+++ src/params.js
@@ -1,19 +1,18 @@
 const DEFAULT_QUALITY = 40;
 
 function params(req, res, next) {
-  const { url, jpeg, bw, l } = req.query;
+  const url = req.url;
+  const cleanedUrl = url.replace(/^\/+|\/+$/g, '');
+  const [l, w, bw, jpeg] = cleanedUrl.split('/');
 
-  if (!url) {
-    return res.end('bandwidth-hero-proxy');
-  }
-
-  const urls = Array.isArray(url) ? url.join('&url=') : url;
-  const cleanedUrl = urls.replace(/http:\/\/1\.1\.\d\.\d\/bmi\/(https?:\/\/)?/i, 'http://');
-
-  req.params.url = cleanedUrl;
-  req.params.webp = !jpeg;
-  req.params.grayscale = bw !== '0';
-  req.params.quality = parseInt(l, 10) || DEFAULT_QUALITY;
+  req.params = {
+    url: cleanedUrl,
+    webp: !jpeg,
+    grayscale: bw !== '0',
+    quality: parseInt(l, 10) || DEFAULT_QUALITY,
+    originType: req.headers['content-type'] || '', // Default to empty string if header is missing
+    originSize: parseInt(req.headers['content-length'], 10) || 0, // Default to 0 if header is missing or invalid
+  };
 
   next();
 }
